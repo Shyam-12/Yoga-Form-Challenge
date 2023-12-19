7a4a5aed94e32ee80f7675657829d9da1ae044e1
Shyam-12 <shyamprasad1248@gmail.com>
2023-12-19 22:06:04 +0530

    Adding all the codes of the project

---
+++ server/index.js
@@ -0,0 +1,31 @@
+// index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+// routes
+const enrollmentRoutes = require('./src/routes/enrollmentRoutes');
+
+// set up mongo
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,   
+    } 
+);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api', enrollmentRoutes);
+
+app.listen(PORT, (req, res) => {
+    console.log(`Listening on port ${PORT}`);
+});
